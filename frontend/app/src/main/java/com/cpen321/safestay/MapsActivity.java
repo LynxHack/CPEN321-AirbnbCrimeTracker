9709f6098325a606b67d8142c4387b7415773fdd
SJ097 <SJ097@users.noreply.github.com>
2019-11-01 03:20:55 -0700

    Search implemented, autocomplete still required

--- frontend/app/src/main/java/com/cpen321/safestay/MapsActivity.java
+++ frontend/app/src/main/java/com/cpen321/safestay/MapsActivity.java
@@ -101,7 +101,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             @Override
             public void onSearchConfirmed(CharSequence text) {
-                startSearch(text.toString(), true, null, true);
+                //startSearch(text.toString(), true, null, true);
+                searchCity(text.toString());
             }
 
             @Override
@@ -125,7 +126,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 FindAutocompletePredictionsRequest predictionsRequest = FindAutocompletePredictionsRequest.builder()
-                        .setTypeFilter(TypeFilter.ADDRESS).setSessionToken(token).setQuery(charSequence.toString())
+                        .setTypeFilter(TypeFilter.CITIES).setSessionToken(token).setQuery(charSequence.toString())
                         .build();
                 placesClient.findAutocompletePredictions(predictionsRequest).addOnCompleteListener(new OnCompleteListener<FindAutocompletePredictionsResponse>() {
                     @Override
@@ -291,10 +292,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-    private void searchCity() {
-
-        String city = "burnaby";
-
+    private void searchCity(String city) {
         // Replace spaces with space ASCII code
         String searchURL = googleURL + city.replace(" ", "%20") + googleSearchKey;
 
@@ -313,7 +311,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                             //problem with receiving JSONObject
                             //OR
                             //problem with extracting info from JSONObject
-                            Toast.makeText(getApplicationContext(), "JSON Exception in login activity!!", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getApplicationContext(), "Invalid city", Toast.LENGTH_SHORT).show();
                             e.printStackTrace();
                         }
                     }
@@ -321,7 +319,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(getApplicationContext(), "Some kind of error", Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(), "Invalid city", Toast.LENGTH_SHORT).show();
                         if (error == null || error.networkResponse == null) {
                             return;
                         }
