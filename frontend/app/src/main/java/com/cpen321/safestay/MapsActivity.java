73fc2fb04ae4d0fcfaf6441e494d1aa08f0d393c
Lingye Kong <lingye@dhcp-128-189-92-82.ubcsecure.wireless.ubc.ca>
2019-11-03 14:30:20 -0800

    change back to safety_index and person_capacity, otherwise application will be broken

--- frontend/app/src/main/java/com/cpen321/safestay/MapsActivity.java
+++ frontend/app/src/main/java/com/cpen321/safestay/MapsActivity.java
@@ -206,7 +206,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                                         if (!markerList.contains(coords)) {
 
-                                            int safety_index = current.getInt("safetyIndex");
+                                            int safety_index = current.getInt("safety_index");
                                             float markerColour;
 
                                             if (safety_index > 6)
@@ -217,7 +217,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                                             mMap.addMarker(new MarkerOptions().position(coords)
                                                     .title(current.getString("name")
-                                                            + "\nMax Occupancy: " + current.getInt("personCapacity")
+                                                            + "\nMax Occupancy: " + current.getInt("person_capacity")
                                                             + "\nRating: " + current.getDouble("star_rating") + " Stars")
                                                     .icon(BitmapDescriptorFactory.defaultMarker(markerColour)));
 
