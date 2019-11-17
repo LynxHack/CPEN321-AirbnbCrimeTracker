51c25d8d7b1867f7e8601ecd9e2e406acafcbb14
Lingye Kong <lingye@Lingyes-MacBook-Pro.local>
2019-11-17 02:14:14 -0800

    draft test

--- frontend/app/src/androidTest/java/com/cpen321/safestay/ExampleInstrumentedTest.java
+++ frontend/app/src/androidTest/java/com/cpen321/safestay/ExampleInstrumentedTest.java
@@ -2,13 +2,18 @@ package com.cpen321.safestay;
 
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,11 +22,26 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+
+//    @Rule
+//    public ActivityTestRule<MapsActivity> activityRule
+//            = new ActivityTestRule<>(MapsActivity.class);
+
+    @Before
+    public void startApp() {
+    }
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        Context appContext = getInstrumentation().getTargetContext();
 
         assertEquals("com.cpen321.safestay", appContext.getPackageName());
+    }
+    @Test
+    public void googleMapBasicView() {
+        onView(withId(R.id.map))
+                .check(matches(isDisplayed()));
+
     }
 }
